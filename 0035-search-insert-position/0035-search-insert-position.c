ee1d929f8dfc1469105945697ba900f8e03a2cff
Ahmane Mohamed Amine <75282612+A-M-Amine@users.noreply.github.com>
2022-11-05 00:37:24 +0100

    Time: 3 ms (96.96%), Space: 6 MB (53.80%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,20 @@
+int searchInsert(int* nums, int numsSize, int target){
+    
+    if(target < nums[0]) {
+            return 0;
+    }
+
+    if(target > nums[numsSize - 1]) {
+        return numsSize;
+    }
+
+
+    for(int i = 0; i < numsSize; i++) {
+        if(target <= nums[i]) {
+            return i;
+        }
+    }
+    
+    return 0;
+
+}
