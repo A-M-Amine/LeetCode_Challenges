8118fb760ea2adae2098e12761e81ac60d749b5d
Ahmane Mohamed Amine <75282612+A-M-Amine@users.noreply.github.com>
2023-09-21 22:51:14 +0200

    Time: 47 ms (26.70%), Space: 16.3 MB (88.48%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,38 +1,17 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         
-        frequency = {}
         pos = 0
         prefix = ""
-        if len(strs) == 1:
-            return strs[0]        
-        
-        while strs:
 
+        while True:
+
+            temp = strs[0][:pos+1]
             for word in strs:
-                if len(word) > 0 and pos < len(word):
-                    letter = word[pos]
-                    frequency[letter] = frequency.get(letter, 0) + 1
+                if not word.startswith(temp) or len(word) == len(prefix):
+                    return prefix
 
-            max = ("", 0)
-            for key, val in frequency.items():
-                if val > max[1]:
-                    max = (key, val)
-
-            if max[1] <= 1:
-                break
-            
-            old = prefix
-            prefix += max[0]
-
-            for word in strs:
-                if not word.startswith(prefix):
-                    return old
-
+            prefix = strs[0][:pos+1]
             pos += 1
-            frequency = {}
-            
-
-        return prefix
         
         
